b07df3aa560effd26b0a9b1301e0bbb48fc1f9f6
Admin <Admin@DESKTOP-DFAIJ8K>
2024-10-29 11:33:59 +0200

    Add project files.

---
+++ ControllerViewExample/Controllers/StuffController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerViewExample.Controllers
+{
+    public class StuffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Nav()
+        {
+            return View();
+        }
+    }
+}
